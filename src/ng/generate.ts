493dcb58199ebf801ed88834045b97c9aa78ee5a
Ashwin Sureshkumar <ashwin.sureshkumar@gmail.com>
2017-02-17 06:51:01 -0500

    Added type module to ng generate - types

--- src/ng/generate.ts
+++ src/ng/generate.ts
@@ -5,7 +5,7 @@ import { runNgCommand } from './run';
 
 export var nggenerate = vscode.commands.registerCommand('extension.ngGenerate', () => {
         let param: string[] = ['generate']
-        let items = ['component', 'directive', 'route', 'pipe', 'service'];
+        let items = ['component', 'directive', 'route', 'pipe', 'service', 'module'];
         let options = { matchOnDescription: false, placeHolder: "select Type" };
         vscode.window.showQuickPick(items, options).then((data) => {
             param.push(data);
