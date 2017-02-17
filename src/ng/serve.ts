d423de33e241974e6622465d2631024301f2b6dd
Ashwin Sureshkumar <ashwin.sureshkumar@gmail.com>
2017-02-17 07:14:22 -0500

    Add optional parameter input box to ng serve

--- src/ng/serve.ts
+++ src/ng/serve.ts
@@ -4,5 +4,9 @@ import * as vscode from 'vscode';
 import { runNgCommand } from './run'
 
 export var ngserve = vscode.commands.registerCommand('extension.ngServe', () => {
-    runNgCommand(['serve'], true);
+    vscode.window.showInputBox({ placeHolder: 'Enter optional paramaters if any, example --port 4201 --host 0.0.0.0' })
+                 .then((optionalParams = '') => {
+                    runNgCommand([`serve ${optionalParams}`], true);
+                 });
+                
 });
